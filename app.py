b10aefda0880bbd3895c66ea5f58561172a4bf5d
Tnkma <onwusilikegodswill@gmail.com>
2024-03-28 12:58:49 +0100

    adding up

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from functions import valid_login
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -36,19 +37,20 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    # We create a object of the LoginForm class
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        if valid_login(form.email.data, form.password.data):
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('plumber_dashboard'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check username and password', 'danger')    
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/plumber")
--- functions.py
+++ functions.py
@@ -13,3 +13,10 @@ def validate_phone(form, field):
             raise ValueError("Invalid phone number")
     except (phonenumberutil.NumberParseException, ValueError):
         raise ValidationError("Invalid phone number format")
+    
+def valid_login(username, password):
+    # Replace this with your actual authentication logic, such as querying a database
+    if username == 'admin@gmail.com' and password == 'password':
+        return True
+    else:
+        return False
